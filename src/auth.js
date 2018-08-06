d39a30e739a0307ab9ce13dc997bb8aa6a6688ef
Andrey <popov.a83@bk.ru>
2018-08-06 11:47:48 +0200

    worked google & facebook auth

---
+++ src/auth.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase'
+import firebaseui from 'firebaseui';
+
+const config = {
+  apiKey: "AIzaSyDsLHM0LsBpI4AyLV5-T-1BiOlW3K61fvs",
+  authDomain: "gauth-vue.firebaseapp.com",
+  databaseURL: "https://gauth-vue.firebaseio.com",
+  projectId: "gauth-vue",
+  storageBucket: "gauth-vue.appspot.com",
+  messagingSenderId: "884141954620"
+};
+
+const auth = {
+  context: null,
+  uiConfig: null,
+  ui: null,
+
+  init(context) {
+    this.context = context;
+
+    firebase.initializeApp(config);
+    this.uiConfig = {
+      signInSuccessUrl: 'dashboard',
+      signInOptions: [
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ]
+    }
+    this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    firebase.auth().onAuthStateChanged((user) => {
+      this.context.$store.dispatch('user/setCurrentUser')
+
+      let requireAuth = this.context.$route.matched.some(record => record.meta.requireAuth)
+      let guestOnly = this.context.$route.matched.some(record => record.meta.guestOnly)
+
+      if(requireAuth && !user) this.context.$router.push('auth')
+      else if (guestOnly && user) this.context.$router.push('dashboard')
+    });
+  },
+  authForm(container) {
+    this.ui.start(container, this.uiConfig);
+  },
+  user() {
+    return this.context ? firebase.auth().currentUser : null;
+  },
+  logout() {
+    firebase.auth().signOut();
+  }
+}
+
+export default auth;
